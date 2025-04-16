5e0e7cbad77fa9d73f09eafcab87af34c362e96e
Filipe <filipemouramen@gmail.com>
2025-04-16 17:22:05 -0300

    01

---
+++ exercicioo13/FilaCircular.cs
@@ -0,0 +1,93 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace exercicioo13
+{
+    class FilaCircular
+    {
+        private int[] fila;
+        private int frente;
+        private int tras;
+        private int tamanho;
+        private int capacidade;
+
+        public FilaCircular(int capacidade)
+        {
+            this.capacidade = capacidade;
+            fila = new int[capacidade];
+            frente = -1;
+            tras = -1;
+            tamanho = 0;
+        }
+
+        public bool EstaCheia()
+        {
+            return tamanho == capacidade;
+        }
+
+        public bool EstaVazia()
+        {
+            return tamanho == 0;
+        }
+
+        public void Enfileirar(int valor)
+        {
+            if (EstaCheia())
+            {
+                Console.WriteLine("Fila cheia");
+                return;
+            }
+
+            if (EstaVazia())
+            {
+                frente = 0;
+            }
+
+            tras = (tras + 1) % capacidade;
+            fila[tras] = valor;
+            tamanho++;
+            Console.WriteLine($"Enfileirado: {valor}");
+        }
+
+        public void Desenfileirar()
+        {
+            if (EstaVazia())
+            {
+                Console.WriteLine("Fila vazia");
+                return;
+            }
+
+            int valor = fila[frente];
+            frente = (frente + 1) % capacidade;
+            tamanho--;
+
+            if (tamanho == 0)
+            {
+                frente = -1;
+                tras = -1;
+            }
+
+            Console.WriteLine($"Desenfileirado: {valor}");
+        }
+
+        public void MostrarFila()
+        {
+            if (EstaVazia())
+            {
+                Console.WriteLine("Fila vazia");
+                return;
+            }
+
+            Console.Write("Fila: ");
+            for (int i = 0; i < tamanho; i++)
+            {
+                int index = (frente + i) % capacidade;
+                Console.Write(fila[index] + " ");
+            }
+            Console.WriteLine();
+        }
+    }
+}
---
+++ exercicioo13/Program.cs
@@ -0,0 +1,46 @@
+﻿using exercicioo13;
+
+Console.Write("Digite a capacidade da fila: ");
+int capacidade = int.Parse(Console.ReadLine());
+
+FilaCircular fila = new FilaCircular(capacidade);
+int opcao;
+
+do
+{
+    Console.WriteLine("--------------------");
+    Console.WriteLine("1. Enfileirar");
+    Console.WriteLine("2. Desenfileirar");
+    Console.WriteLine("3. Mostrar fila");
+    Console.WriteLine("0. Sair");
+    Console.WriteLine("--------------------");
+    Console.WriteLine();
+    Console.Write("Escolha uma opção: ");
+    Console.WriteLine();
+    opcao = int.Parse(Console.ReadLine());
+
+    switch (opcao)
+    {
+        case 1:
+            Console.Write("Digite um valor para enfileirar: ");
+            int valor = int.Parse(Console.ReadLine());
+            fila.Enfileirar(valor);
+            break;
+
+        case 2:
+            fila.Desenfileirar();
+            break;
+
+        case 3:
+            fila.MostrarFila();
+            break;
+
+        case 0:
+            Console.WriteLine("Saindo...");
+            break;
+
+        default:
+            Console.WriteLine("Opção inválida");
+            break;
+    }
+} while (opcao != 0);
