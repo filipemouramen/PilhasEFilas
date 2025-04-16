7d75eb87c56dc00e1708f58430b3361b02ea0b1c
Filipe <filipemouramen@gmail.com>
2025-04-16 17:38:18 -0300

    17

---
+++ Exercicio17/Impressora.cs
@@ -0,0 +1,73 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Exercicio17
+{    
+class Documento
+    {
+        public string Nome { get; set; }
+        public string Prioridade { get; set; } 
+
+        public Documento(string nome, string prioridade)
+        {
+            Nome = nome;
+            Prioridade = prioridade.ToLower();
+        }
+    }
+
+    class GerenciadorImpressao
+    {
+        private Queue<Documento> filaAltaPrioridade = new Queue<Documento>();
+        private Queue<Documento> filaBaixaPrioridade = new Queue<Documento>();
+
+        public void AdicionarDocumento(Documento doc)
+        {
+            if (doc.Prioridade == "alta")
+            {
+                filaAltaPrioridade.Enqueue(doc);
+            }
+            else
+            {
+                filaBaixaPrioridade.Enqueue(doc);
+            }
+
+            Console.WriteLine($"Documento {doc.Nome} com prioridade {doc.Prioridade} adicionado à fila.");
+        }
+
+        public void ImprimirProximo()
+        {
+            if (filaAltaPrioridade.Count > 0)
+            {
+                Documento doc = filaAltaPrioridade.Dequeue();
+                Console.WriteLine($"Imprimindo (prioridade alta): {doc.Nome}");
+            }
+            else if (filaBaixaPrioridade.Count > 0)
+            {
+                Documento doc = filaBaixaPrioridade.Dequeue();
+                Console.WriteLine($"Imprimindo (prioridade baixa): {doc.Nome}");
+            }
+            else
+            {
+                Console.WriteLine("Nenhum documento na fila.");
+            }
+        }
+
+        public void MostrarFilas()
+        {
+            Console.WriteLine("---Fila de Prioridade Alta---");
+            foreach (var doc in filaAltaPrioridade)
+            {
+                Console.WriteLine($"{doc.Nome}");
+            }
+
+            Console.WriteLine("---Fila de Prioridade Baixa---");
+            foreach (var doc in filaBaixaPrioridade)
+            {
+                Console.WriteLine($"{doc.Nome}");
+            }
+        }
+    }
+}
---
+++ Exercicio17/Program.cs
@@ -0,0 +1,47 @@
+﻿using Exercicio17;
+
+GerenciadorImpressao gerenciador = new GerenciadorImpressao();
+int opcao;
+
+do
+{
+    Console.WriteLine();
+    Console.WriteLine("--------Menu Impressora-------");
+    Console.WriteLine("1. Adicionar documento");
+    Console.WriteLine("2. Imprimir próximo documento");
+    Console.WriteLine("3. Mostrar filas");
+    Console.WriteLine("0. Sair");
+    Console.WriteLine("------------------------------");
+    Console.WriteLine();
+    Console.Write("Escolha uma opção: ");
+    opcao = int.Parse(Console.ReadLine());
+
+    switch (opcao)
+    {
+        case 1:
+            Console.Write("Digite o nome do documento: ");
+            string nome = Console.ReadLine();
+
+            Console.Write("Digite a prioridade do documento(alta/baixa): ");
+            string prioridade = Console.ReadLine();
+            gerenciador.AdicionarDocumento(new Documento(nome, prioridade));
+            break;
+
+        case 2:
+            gerenciador.ImprimirProximo();
+            break;
+
+        case 3:
+            gerenciador.MostrarFilas();
+            break;
+
+        case 0:
+            Console.WriteLine("Saindo...");
+            break;
+
+        default:
+            Console.WriteLine("Opção inválida.");
+            break;
+    }
+
+} while (opcao != 0);
